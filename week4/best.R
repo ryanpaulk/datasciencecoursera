4c41903b27dac6ff297ad82cef48e9e33eb2ac84
ryanpaulk <ryanpaulk@gmail.com>
2020-12-09 21:05:13 -0600

    Getting started

--- week4/best.R
+++ week4/best.R
@@ -1,6 +1,28 @@
 best <- function(state, outcome) {
   ## Read outcome data
   ## Check that state and outcome are valid
+  ## is "state" found in Column = "State"?
+  x = state %in% outcome$State
+  ## is cause of death one of Heart Failure, Attack, or Pneumonia?
+  y = outcome %in% c("Heart Failure", "Heart Attack", "Pneumonia")
+  if (x == FALSE) {
+    stop("Invalid State")
+  }
+  if (y == FALSE) {
+    stop("Invalid Outcome")
+  }
+  ##If cause of death is valid, direct data to specific column to use
+  if (outcome == "Heart Attack") {
+    death <- outcome$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack
+  }
+  if (outcome == "Heart Failure") {
+    death <- outcome$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure
+  }
+  if (outcome == "Pneumonia") {
+    death <- outcome$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia
+  }
+  }
+  }
   ## Return hospital name in that state with lowest 30-day death
   ## rate
 }
